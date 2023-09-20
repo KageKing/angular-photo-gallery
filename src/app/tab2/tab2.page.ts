57125fbf3e3db652c36878aabcf85734a58fb709
kage <107029373+KageKing@users.noreply.github.com>
2023-09-20 10:36:18 +0200

    angular

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  constructor(public photoService: PhotoService) {} // Use 'public' here
 
-  constructor() {}
+  ngOnInit() {
+    // Initialize the property here
+    // this.someVariable = 'Initial Value'; // Remove this line if 'someVariable' is not used
+  }
 
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
